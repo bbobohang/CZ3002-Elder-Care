5c2c9eae35a22f11a749720dc67dcff6f9fb99d4
bbobohang <bbobohang@gmail.com>
2022-08-30 12:24:44 +0800

    Edited model

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	role: {
+		type: String,
+		default: 'patient',
+	},
 });
 
 module.exports = mongoose.model('user', UserSchema);
