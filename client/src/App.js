f739d298af84727e14dd69665d93f802c9831518
bbobohang <bbobohang@gmail.com>
2022-09-02 14:58:57 +0800

    Patient HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './Auth/Register';
 import Login from './Auth/Login';
 import PrivateRoute from './Routes/PrivateRoute';
-import Home from './Components/Home';
+import PatientHome from './Components/patient/PatientHome';
 import PatientRecord from './Components/PatientRecord';
+
+import './App.css';
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path='/login' element={<Login />} />
 				<Route path='/register' element={<Register />} />
-				<Route path='home' element={<PrivateRoute component={Home} />} />
+				<Route path='home' element={<PrivateRoute component={PatientHome} />} />
 				<Route path='record' element={<PrivateRoute component={PatientRecord} />} />
 			</Routes>
 		</BrowserRouter>
