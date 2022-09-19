8e3f060bcaf8e4e0e38ff33421c0dce6209b470f
bbobohang <bbobohang@gmail.com>
2022-09-19 18:16:54 +0800

    401 page & rejection css

--- client/src/Components/patient/Navbar.js
+++ client/src/Components/patient/Navbar.js
@@ -6,64 +6,111 @@ import axios from 'axios';
 import './Navbar.css';
 import { useNavigate } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ role }) => {
 	const navigate = useNavigate();
 	const logout = () => {
 		axios.post(`/api/auth/logout`).then((response) => {
 			console.log(response);
 		});
 
-		navigate('/login');
+		navigate('/');
 	};
 
-	return (
-		<div className='row'>
-			<nav className='nav'>
-				<div className='navWrapper'>
-					<div className='navLogo'>
-						<img src={Logo} alt='Logo' />
-					</div>
-					<ul className='navList'>
-						<li className='navItem'>
-							<a className='listItem' href='/phome'>
-								Home
-							</a>
-						</li>
+	//Doctor Navbar
+	if (role === 'doctor') {
+		return (
+			<div className='row'>
+				<nav className='nav'>
+					<div className='navWrapper'>
+						<div className='navLogo'>
+							<img src={Logo} alt='Logo' />
+						</div>
+						<ul className='navList'>
+							<li className='navItem'>
+								<a className='listItem' href='/dhome'>
+									Dashboard
+								</a>
+							</li>
 
-						<li className='navItem'>
-							<a className='listItem' href='#projects'>
-								Appointments
-							</a>
-						</li>
-						<li className='navItem'>
-							<a className='listItem' href='/pmed/status'>
-								Med Orders
-							</a>
-						</li>
-						<li className='navItem'>
-							<a className='listItem' href='#experiences'>
-								Wallet
-							</a>
-						</li>
+							<li className='navItem'>
+								<a className='listItem' href='#projects'>
+									Appointments
+								</a>
+							</li>
+							<li className='navItem'>
+								<a className='listItem' href='/dmed/accept'>
+									Accept Meds
+								</a>
+							</li>
 
-						<li className='navItem'>
-							<a className='listItem'>
-								<div class='navigation'>
-									<CgProfile size={25} />
-									<div class='navigation-content'>
-										<a href='/profile'>Profile</a>
-										<a href='#' onClick={logout}>
-											Log Out
-										</a>
+							<li className='navItem'>
+								<a className='listItem'>
+									<div class='navigation'>
+										<CgProfile size={25} />
+										<div class='navigation-content'>
+											<a href='/profile'>Profile</a>
+											<a href='#' onClick={logout}>
+												Log Out
+											</a>
+										</div>
 									</div>
-								</div>
-							</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-	);
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		);
+	} else {
+		return (
+			<div className='row'>
+				<nav className='nav'>
+					<div className='navWrapper'>
+						<div className='navLogo'>
+							<img src={Logo} alt='Logo' />
+						</div>
+						<ul className='navList'>
+							<li className='navItem'>
+								<a className='listItem' href='/phome'>
+									Home
+								</a>
+							</li>
+
+							<li className='navItem'>
+								<a className='listItem' href='#projects'>
+									Appointments
+								</a>
+							</li>
+							<li className='navItem'>
+								<a className='listItem' href='/pmed/status'>
+									Med Orders
+								</a>
+							</li>
+							<li className='navItem'>
+								<a className='listItem' href='#experiences'>
+									Wallet
+								</a>
+							</li>
+
+							<li className='navItem'>
+								<a className='listItem'>
+									<div class='navigation'>
+										<CgProfile size={25} />
+										<div class='navigation-content'>
+											<a href='/profile'>Profile</a>
+											<a href='#' onClick={logout}>
+												Log Out
+											</a>
+										</div>
+									</div>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		);
+	}
 };
 
 export default Navbar;
--- client/src/Routes/PrivateRoute.js
+++ client/src/Routes/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component }) => {
+const PrivateRoute = ({ component: Component, role }) => {
 	const [isAuthenticated, setAuthenticated] = useState({});
 	const requestOptions = {
 		method: 'GET',
@@ -16,8 +16,9 @@ const PrivateRoute = ({ component: Component }) => {
 				console.log(data);
 			});
 	}, []);
-	if (isAuthenticated.errors) return <Navigate to='/login' />;
-
+	if (isAuthenticated.errors) return <Navigate to='/' />;
+	if (role === 'doctor' && isAuthenticated.role === 'patient')
+		return <Navigate to='/401' />;
 	return <Component />;
 };
 
