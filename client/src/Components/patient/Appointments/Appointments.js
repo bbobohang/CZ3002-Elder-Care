e14cefce0b2aaa94a89077ddf6349eb076521eae
roydonauyr <roydonauyr@gmail.com>
2022-10-07 11:32:02 +0800

    Added some parts of appointment

--- client/src/Components/patient/Appointments/Appointments.js
+++ client/src/Components/patient/Appointments/Appointments.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import axios from "axios";
 
 import "../Helper Components/Modal.css";
 import "./Appointment.css";
@@ -10,27 +11,77 @@ import "./Appointment.css";
 const localizer = momentLocalizer(moment);
 
 const Appointments = () => {
-  const myEvents = [
+  //const [tempAppointment, setTempAppointment] = useState([]);
+  const [myEvents, setMyEvents] = useState([{ hello: 1 }]);
+
+  const tempMyEvents = [
     {
-      title: "Test",
-      desc: "Testing longer description",
-      type: "HomeConsult",
-      start: new Date(2022, 8, 24, 8, 0),
-      end: new Date(2022, 8, 24, 15, 0),
-    },
-    {
-      title: "test",
-      type: "MedicineDelivery",
-      start: new Date(2022, 8, 24, 9, 0),
-      end: new Date(2022, 8, 24, 15, 0),
-    },
-    {
-      title: "test",
-      type: "TeleDoctor",
-      start: new Date(2022, 8, 25, 10, 0),
-      end: new Date(2022, 8, 25, 15, 0),
+      title: "TeleDoctor Appointment",
+      year: 2022,
+      month: 9,
+      day: 14,
+      hour: 11,
+      end_hour: 12,
+      min: 0,
+      allDay: false,
+      // start: Date(2022, 8, 14, 11, 0),
+      // end: Date(2022, 8, 14, 12, 0),
     },
   ];
+
+  const changeFormat = (eventList) => {
+    return eventList.map((event) => {
+      if (event["year"] == null) {
+        event["start"] = new Date(0, 0, 0, 0, 0);
+        event["end"] = new Date(0, 0, 0, 0, 0);
+      } else {
+        event["start"] = new Date(
+          event["year"],
+          event["month"] - 1,
+          event["day"],
+          event["hour"],
+          event["min"]
+        );
+        console.log(event["start"]);
+        event["end"] = new Date(
+          event["year"],
+          event["month"] - 1,
+          event["day"],
+          event["end_hour"],
+          event["min"]
+        );
+      }
+    });
+  };
+
+  useEffect(() => {
+    changeFormat(tempMyEvents);
+    setMyEvents([{ hello: 2 }]);
+    console.log(myEvents);
+    //setMyEvents(["tempMyEvents"]);
+  }, []);
+
+  // const myEvents = [
+  //   {
+  //     title: "Test",
+  //     desc: "Testing longer description",
+  //     type: "HomeConsult",
+  //     start: new Date("2022", "8", "24", "8", "0"),
+  //     end: new Date("2022", 8, 24, 15, 0),
+  //   },
+  //   {
+  //     title: "test",
+  //     type: "MedicineDelivery",
+  //     start: new Date(2022, 8, 24, 9, 0),
+  //     end: new Date(2022, 8, 24, 15, 0),
+  //   },
+  //   {
+  //     title: "test",
+  //     type: "TeleDoctor",
+  //     start: new Date(2022, 8, 25, 10, 0),
+  //     end: new Date(2022, 8, 25, 15, 0),
+  //   },
+  // ];
 
   const [modalState, setModalState] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState(undefined);
@@ -74,11 +125,11 @@ const Appointments = () => {
           style={{ height: 500 }}
           eventPropGetter={(myEvents) => {
             var backgroundColor = "blue";
-            if (myEvents.type == "HomeConsult") {
+            if (myEvents.title == "Medication Delivery") {
               backgroundColor = "#388E3C";
-            } else if (myEvents.type == "MedicineDelivery") {
+            } else if (myEvents.type == "Home Doctor Appointment") {
               backgroundColor = "#9575CD";
-            } else if (myEvents.type == "TeleDoctor") {
+            } else if (myEvents.type == "TeleDoctor Appointment") {
               backgroundColor = "#0288D1";
             }
             return { style: { backgroundColor } };
